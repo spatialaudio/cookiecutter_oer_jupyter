a843a524188bc7010e185986f5a5cd3d7547e5b2
Johannes Otto <johannes-otto95@t-online.de>
2024-07-24 10:26:08 +0200

    update hook, add empty requirements.txt

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -16,25 +16,46 @@ def is_github_cli_installed() -> bool:
 
 if {% if cookiecutter.create_git == "Yes" %}True{% else %}False{% endif %}:
 
-	if is_git_installed:
+	if is_git_installed():
 
-		#subprocess.call(['git', 'init'])
-		#subprocess.call(['git', 'add', '*'])
-		#subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+		subprocess.run(['git', 'init'])
+		subprocess.run(['git', 'add', '*'])
+		subprocess.run(['git', 'commit', '-m', 'Initial commit'])
 		print('Initialized Git')
+
+		if {% if cookiecutter.create_remote_and_push == "Yes" %}True{% else %}False{% endif %}:
+
+			if is_github_cli_installed():
+
+				if {{cookiecutter.github_organization}} != 'none' and {{cookiecutter.github_organization}} != 'None':
+					new_repo = {{cookiecutter.github_organization}} +'/'+ {{cookiecutter.directory_name}}
+				else:
+					new_repo = {{cookiecutter.directory_name}}
+
+				subprocess.run(['gh', 'repo', 'create', new_repo, '--'+{{cookiecutter.git_visibility}}, '--source', {{cookiecutter.directory_name}}, '--push'])
+				print('Created GitHub remote and pushed local repository')
+
+			else:
+				print('No GitHub CLI installation found - GitHub initialization and push local->remote skipped!')
+
+		if {% if cookiecutter.push_existing_remote == "Yes" %}True{% else %}False{% endif %}:
+				
+			if is_github_cli_installed():
+
+				if {{cookiecutter.github_organization}} != 'none' and {{cookiecutter.github_organization}} != 'None':
+					new_repo = {{cookiecutter.github_organization}} +'/'+ {{cookiecutter.directory_name}}
+				else:
+					new_repo = {{cookiecutter.directory_name}}
+
+				subprocess.run(['git', 'remote', 'add', 'origin', {{cookiecutter.git_remote}}])
+				subprocess.run(['git', 'push'])
+				print('Created GitHub remote and pushed local repository')
+
+			else:
+				print('No GitHub CLI installation found - GitHub initialization and push local->remote skipped!')
 
 	else:
 		print('No git installation found - git initialization skipped!')
-
-	if {% if cookiecutter.create_remote_and_push == "Yes" %}True{% else %}False{% endif %}:
-
-		if is_github_cli_installed:
-
-			#subprocess.call(['gh', 'repo', 'create', '--'+{{cookiecutter.git_visibility}}, '--source', '--push'])
-			print('Created GitHub remote and pushed local repository')
-
-		else:
-			print('No GitHub CLI installation found - GitHub initialization and push local->remote skipped!')
 
 else:
 	print('No git initialized')
