2a43c03fdae7282c2f4ced5e12b92fe774f9bbf6
Johannes Otto <johannes-otto95@t-online.de>
2024-08-06 13:52:06 +0200

    Split license, GitHub functionality, CI files WIP

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,17 @@
 import subprocess
+import shutil
+import os
+
+
+
+def check_directory() -> bool:
+
+	cwd = os.getcwd()
+	
+	if os.path.basename(cwd) == '{{cookiecutter.directory_name}}':
+		return True
+	else:
+		return False
 
 def is_git_installed() -> bool:
     try:
@@ -13,6 +26,60 @@ def is_github_cli_installed() -> bool:
         return True
     except Exception:
         return False
+    
+def delete_in_list(list):
+	for del_path in list:
+		if os.path.exists(del_path):
+			if os.path.isdir(del_path):
+				shutil.rmtree(del_path)
+			else:
+				os.remove(del_path)
+				
+    
+
+# cleanup
+if check_directory():
+
+	# remove unneeded files
+	cu_files = ['.ipynb_checkpoints']
+	delete_in_list(cu_files)
+
+	# remove ci files if not wanted
+	if {% if cookiecutter.include_ci == "No" %}True{% else %}False{% endif %}:
+		ci_files = ['.github', 'ci', '.travis.yml']
+		delete_in_list(ci_files)
+else:
+	raise Exception('Wrong working directory!')
+
+# importing notebook collection
+if {% if cookiecutter.import_notebooks == "Yes" %}True{% else %}False{% endif %}:
+
+	del_files = ['index.ipynb', '.DS_Store']
+	del_folders = ['introduction','data']
+	nb_col_path = '{{cookiecutter.notebook_collection_path}}'
+	cwd = os.getcwd()
+	nb_col = os.listdir(nb_col_path)
+
+	if check_directory():
+		
+		# remove placeholder files and folders
+		for del_path in del_files:
+			if os.path.exists(del_path):
+				os.remove(del_path)
+
+		for del_path in del_folders:
+			if os.path.exists(del_path):
+				shutil.rmtree(del_path)
+			
+		# copy notebook collection
+		for source in nb_col:
+			if os.path.isdir(os.path.join(nb_col_path, source)):
+				shutil.copytree(os.path.join(nb_col_path, source), os.path.join(cwd, source), dirs_exist_ok=True)
+			else:
+				shutil.copy2(os.path.join(nb_col_path, source), os.path.join(cwd, source))
+
+	else:
+		raise Exception('Wrong working directory!')
 
 if {% if cookiecutter.create_git == "Yes" %}True{% else %}False{% endif %}:
 
